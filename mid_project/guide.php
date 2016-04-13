5d893b32227136c117567b51fad0db5de1efb352
second9040 <second9040@gmail.com>
2016-04-13 23:45:19 +0800

    guide.php - 會跑掉 還要修

--- mid_project/guide.php
+++ mid_project/guide.php
@@ -21,32 +21,7 @@ session_start();
   <header>
       <?php require_once './header.php'; ?>
   </header>
-  <div class="container">
-      <nav class="navbar">
-        <div class="navbar-brand">
-            <img src="img/logo.png">
-            <a id="brand-fixed" href="index.html">北科Ｉ美食</a>
-        </div>
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"
-                    onclick="click_menu()">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul id="navbar-fixed" class="nav navbar-nav">
-                <li><a href="#">餐廳導覽</a></li>
-                <li><a href="#">餐廳介紹</a></li>
-                <li><a href="#">美味餐點</a></li>
-                <li><a href="#">最新消息</a></li>
-                <li><a href="#">關於我們</a></li>
-            </ul>
-        </div>
-      </nav>
-  </div><!--container-->
-
+  
   <div class="container">
     <img class="titleImg" src="img/guideMain.jpg">
 
