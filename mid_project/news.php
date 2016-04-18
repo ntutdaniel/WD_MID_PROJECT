e946c783aba3e68a07b08abfb506dc646296022f
tangdaniel <s102590041@gmail.com>
2016-04-18 12:19:27 +0800

    no message

---
+++ mid_project/news.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap/bootstrap_3.6.min.css">
+    <link rel="stylesheet" href="css/news.css"/>
+    <link rel="stylesheet" href="css/header.css"/>
+    <link rel="stylesheet" href="css/footer.css"/>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap/bootstrap_3.6.min.js"></script>
+</head>
+<body>
+<header>
+    <?php require_once "./header.php"?>
+</header>
+<article>
+
+</article>
+<footer>
+    <?php require_once "./footer.php"?>
+</footer>
+</body>
+</html>
