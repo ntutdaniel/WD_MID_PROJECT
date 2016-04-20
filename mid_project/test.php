321b81efe98fa26bd56cbc5f44e598ae4fbc7524
second9040 <second9040@gmail.com>
2016-04-20 12:57:00 +0800

    index特效.php
    沒有merge

---
+++ mid_project/test.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
+    <title>test</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/test.css">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap_3.6.min.css">
+    <link rel="stylesheet" href="css/header.css"/>
+    <link rel="stylesheet" href="css/guide.css"/>
+    <link rel="stylesheet" href="css/footer.css"/>
+    <link rel="stylesheet" href="css/index.css"/>
+    <link rel="stylesheet" href="css/animate.css"/>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap/bootstrap_3.6.min.js"></script>
+    <script src="js/header.js"></script>
+    <script src="js/script.js"></script>
+
+  </head>
+
+  <body>
+  <section class="strips">
+  <article class="strips__strip">
+    <div class="strip__content">
+      <a
+      href="http://workforland.azurewebsites.net/wd/mid/index.php">
+      <h1 class="strip__title" data-name="Lorem">北科I美食  </h1>
+      </a>
+     </div>
+  </article>
+
+
+
+
+  <article class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title" data-name="Ipsum">餐廳導覽</h1>
+      <div class="strip__inner-text">
+        <a
+        href="http://workforland.azurewebsites.net/wd/mid/guide.php">
+        <h1 class="strip__title" data-name="Lorem">餐廳導覽  </h1>
+        </a>
+      </div>
+    </div>
+  </article>
+  <article class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title" data-name="Dolor">餐廳介紹</h1>
+      <div class="strip__inner-text">
+        <a
+        href="http://workforland.azurewebsites.net/wd/mid/restaurant_intro.php">
+        <h1 class="strip__title" data-name="Lorem">餐廳介紹  </h1>
+        </a>
+      </div>
+    </div>
+  </article>
+  <article class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title" data-name="Sit">最新消息</h1>
+      <div class="strip__inner-text">
+
+        <a
+        href="http://workforland.azurewebsites.net/wd/mid/News.php">
+        <h1 class="strip__title" data-name="Lorem">最新消息  </h1>
+        </a>
+
+      </div>
+    </div>
+  </article>
+  <article class="strips__strip">
+    <div class="strip__content">
+      <h1 class="strip__title" data-name="Amet">關於我們</h1>
+      <div class="strip__inner-text">
+
+
+        <a
+        href="http://workforland.azurewebsites.net/wd/mid/aboutMe.php">
+        <h1 class="strip__title" data-name="Lorem">關於我們  </h1>
+        </a>
+
+
+
+      </div>
+    </div>
+  </article>
+
+
+  <i class="fa fa-close strip__close"></i>
+</section>
+<script src="js/guide.js"></script>
+<script src="js/test.js"></script>
+
+
+
+
+  </body>
+</html>
